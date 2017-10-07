de916060a1b6e1bbf385e0e844829a814650be65
Veronica K. B. Olsen <code@vkbo.net>
2017-10-08 01:49:45 +0200

    Added characters tree to characters pane

---
+++ nw/gui/charstree.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*
+"""novelWriter Characters Tree Class
+
+ novelWriter – Characters Tree Class
+=====================================
+ Wrapper class for the characters tree
+
+ File History:
+ Created: 2017-10-08 [0.4.0]
+
+"""
+
+import logging as logger
+import nw
+import nw.const as NWC
+import gi
+gi.require_version("Gtk","3.0")
+
+from gi.repository import Gtk
+
+class GuiCharsTree(Gtk.TreeView):
+
+    # Constants
+    COL_TITLE  = 0
+    COL_NUMBER = 1
+    COL_WORDS  = 2
+    COL_NAME   = 3
+    COL_HANDLE = 4
+    
+    def __init__(self, theBook):
+        
+        Gtk.TreeView.__init__(self)
+        
+        # Connect to GUI
+        self.mainConf   = nw.CONFIG
+        self.theBook    = theBook
+        self.iterMap    = {}
+        
+        self.set_name("treeChars")
+        self.set_headers_visible(True)
+        
+        # Core objects
+        self.treeSelect = self.get_selection()
+        self.treeStore  = Gtk.TreeStore(str,str,str,str,str)
+        self.set_model(self.treeStore)
+        
+        # Title
+        self.colTitle   = Gtk.TreeViewColumn(title="Name")
+        self.rendTitle  = Gtk.CellRendererText()
+        self.colTitle.set_expand(True)
+        self.colTitle.pack_start(self.rendTitle,True)
+        self.colTitle.add_attribute(self.rendTitle,"text",0)
+        self.colTitle.set_attributes(self.rendTitle,markup=0)
+        
+        # File Number
+        self.colNumber  = Gtk.TreeViewColumn(title="Files")
+        self.rendNumber = Gtk.CellRendererText()
+        self.colNumber.pack_start(self.rendNumber,False)
+        self.colNumber.add_attribute(self.rendNumber,"text",1)
+        
+        # Word Count
+        self.colWords   = Gtk.TreeViewColumn(title="Words")
+        self.rendWords  = Gtk.CellRendererText()
+        self.colWords.pack_start(self.rendWords,False)
+        self.colWords.add_attribute(self.rendWords,"text",2)
+        self.colWords.set_attributes(self.rendWords,markup=2)
+        
+        # Add to TreeView
+        self.append_column(self.colTitle)
+        self.append_column(self.colNumber)
+        self.append_column(self.colWords)
+        
+        return
+    
+    def loadContent(self):
+        
+        self.treeSelect.set_mode(Gtk.SelectionMode.NONE)
+        self.treeStore.clear()
+        
+        
+        self.expand_all()
+        self.treeSelect.set_mode(Gtk.SelectionMode.SINGLE)
+        
+        return
+    
+# End Class GuiCharsTree
