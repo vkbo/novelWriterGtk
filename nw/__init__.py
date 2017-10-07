1ef7b18f25d5183b0bb6becdcb8e743c20320f16
Veronica K. B. Olsen <code@vkbo.net>
2017-10-07 13:40:06 +0200

    Updated logging tools

--- nw/__init__.py
+++ nw/__init__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*
 """novelWriter Init
 
-novelWriter – Init File
-=======================
-Application initialisation
+ novelWriter – Init File
+=========================
+ Application initialisation
 
-File History:
-Created: 2017-01-10 [0.1.0]
+ File History:
+ Created:   2017-01-10 [0.1.0]
+ Rewritten: 2017-10-03 [0.4.0]
 
 """
 
@@ -18,7 +19,6 @@ gi.require_version("Gtk","3.0")
 
 from gi.repository import Gtk
 from os            import path
-from textwrap      import dedent
 from nw.config     import Config
 from nw.main       import NovelWriter
 
@@ -32,55 +32,94 @@ __maintainer__ = "Veronica Berglyd Olsen"
 __email__      = "code@vkbo.net"
 __status__     = "Development"
 
-# ================================================================================================ #
-# Begin Initialisation
+#
+#  Logging
+# =========
+#  Standard used for logging levels in novelWriter:
+#    CRITICAL  Use for errors that result in termination of the program
+#    ERROR     Use when an action fails, but execution continues
+#    WARNING   When something unexpected, but non-critical happens
+#    INFO      Any useful user information like open, save, exit initiated
+#  ----------- SPAM Threshold : Output above should be minimal -----------------
+#    DEBUG     Use for descriptions of main program flow
+#    VERBOSE   Use for outputting values and program flow details
+#    VVERBOSE  Use for describing what the user does, like clicks and entries
+#
 
-# Logging
-logger  = logging.getLogger(__name__)
+# Adding verbose and vverbose logging levels
+
+VERBOSE  = 9
+VVERBOSE = 8
+logging.addLevelName(VERBOSE, "DEBUG-V")
+logging.addLevelName(VVERBOSE,"DEBUG-VV")
+
+def logVerbose(self, message, *args, **kws):
+    if self.isEnabledFor(VERBOSE):
+        self._log(VERBOSE, message, args, **kws) 
+def logVVerbose(self, message, *args, **kws):
+    if self.isEnabledFor(VVERBOSE):
+        self._log(VVERBOSE, message, args, **kws) 
+
+logging.Logger.verbose  = logVerbose
+logging.Logger.vverbose = logVVerbose
+
+# Initiating logging
+logger = logging.getLogger(__name__)
+
+#
+#  Main Program
+# ==============
+#
 
 # Global Classes
 CONFIG  = Config()
 BUILDER = Gtk.Builder()
 
-# BUILDER.add_from_file(path.join(CONFIG.appPath,"gui.xml"))
 CONFIG.setBuilder(BUILDER)
 # CONFIG.updateRecentList()
-
-#
-# Main program
-#
 
 def main(sysArgs):
     
     # Valid Input Options
-    shortOpt = "hd:ql:v"
+    shortOpt = "hd:qtl:v"
     longOpt  = [
         "help",
         "debug=",
+        "verbose",
+        "vverbose",
         "quiet",
+        "time",
         "logfile=",
         "version",
     ]
     
-    helpMsg = dedent("""
-        novelWriter %s (%s)
-        %s
-        
-        Usage:
-        -h, --help     Print this message.
-        -d, --debug    Debug level. Valid options are DEBUG, INFO, WARN or ERROR.
-        -q, --quiet    Disable output to command line.
-        -l, --logfile  Log file.
-        -v, --version  Print program version and exit.
-        """ % (__version__,__status__,__copyright__)
+    helpMsg = (
+        "novelWriter {version} ({status})\n"
+        "{copyright}\n"
+        "\n"
+        "Usage:\n"
+        " -h, --help      Print this message.\n"
+        " -d, --debug     Debug level. Valid options are DEBUG, INFO, WARN or ERROR.\n"
+        "     --verbose   Increase verbosity of debug.\n"
+        "     --vverbose  Increase verbosity of debug even more.\n"
+        " -q, --quiet     Disable output to command line. Does not affect log file.\n"
+        " -t, --time      Show time stamp in logging output. Adds milliseconds for verbose.\n"
+        " -l, --logfile   Log file.\n"
+        " -v, --version   Print program version and exit.\n"
+    ).format(
+        version   = __version__,
+        status    = __status__,
+        copyright = __copyright__
     )
     
     # Defaults
     debugLevel = logging.WARN
-    debugStr   = "[{asctime}] {levelname:8s}  {message}"
+    debugStr   = "{levelname:8s}  {message}"
+    timeStr    = "[{asctime}] "
     logFile    = ""
     toFile     = False
     toStd      = True
+    showTime   = False
     
     # Parse Options
     try:
@@ -102,7 +141,7 @@ def main(sysArgs):
                 debugLevel = logging.INFO
             elif inArg == "DEBUG":
                 debugLevel = logging.DEBUG
-                debugStr   = "[{asctime}] {lineno:4d}:{name:15s} {levelname:8s}  {message}"
+                debugStr   = "{lineno:4d}:{name:15s} {levelname:8s}  {message}"
             else:
                 print("Invalid debug level")
                 exit(2)
@@ -111,13 +150,21 @@ def main(sysArgs):
             toFile  = True
         elif inOpt in ("-q","--quiet"):
             toStd = False
+        elif inOpt in ("--verbose"):
+            debugLevel = VERBOSE
+            timeStr    = "[{asctime}.{msecs:03.0f}] "
+        elif inOpt in ("--vverbose"):
+            debugLevel = VVERBOSE
+            timeStr    = "[{asctime}.{msecs:03.0f}] "
+        elif inOpt in ("-t","--time"):
+            showTime = True
         elif inOpt in ("-v", "--version"):
             print("makeNovel %s Version %s" % (__status__,__version__))
             exit()
     
     # Set Logging
-    logFmt  = logging.Formatter(fmt=debugStr,datefmt="%Y-%m-%d %H:%M:%S",style="{")
-    # cHandle = logging.StreamHandler()
+    if showTime: debugStr = timeStr+debugStr
+    logFmt = logging.Formatter(fmt=debugStr,datefmt="%Y-%m-%d %H:%M:%S",style="{")
     
     if not logFile == "" and toFile:
         if path.isfile(logFile+".bak"):
